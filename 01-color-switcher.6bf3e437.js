5ae6e15cb77c5c8003ad322d839ec682506579d2
Victor-Lars-Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2023-11-22 21:22:08 +0000

    Deploying to gh-pages from @ Victor-Lars-Petrenko/goit-js-hw-09@f627a0c1e525cd48dd5037b3ff2a99216962a317 🚀

--- 01-color-switcher.6bf3e437.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(a){t.disabled=!0,intervalId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.addEventListener("click",(function(e){t.disabled=!1,clearInterval(intervalId)}))}))}();
-//# sourceMappingURL=01-color-switcher.6bf3e437.js.map
---
+++ 01-color-switcher.81061857.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(e){t.disabled=!0,intervalId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(e){t.disabled=!1,clearInterval(intervalId)}))}();
+//# sourceMappingURL=01-color-switcher.81061857.js.map
