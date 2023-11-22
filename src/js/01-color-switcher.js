c7d7872c92c807e824a1903dd3f04895967a66c6
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2023-11-22 23:13:42 +0200

    done-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,19 +3,20 @@ const stop = document.querySelector('[data-stop]');
 const body = document.querySelector('body')
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-let intervalId;
+// let intervalId;
 
 start.addEventListener('click', evt => {
     start.disabled = true;
     intervalId = setInterval(() => {
         body.style.backgroundColor = getRandomHexColor();
     }, 1000)
+
+    stop.addEventListener('click', e => {
+        start.disabled = false;
+        clearInterval(intervalId);
+    })
 })
 
-stop.addEventListener('click', e => {
-    start.disabled = false;
-    clearInterval(intervalId);
-})
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,7 +3,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector('.form');
 
 function createPromise(position, delay) {
-  return promise = new Promise((resolve, rejected) => {
+  return new Promise((resolve, rejected) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
     if (shouldResolve) {
