f627a0c1e525cd48dd5037b3ff2a99216962a317
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2023-11-22 23:21:42 +0200

    done-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,17 +6,14 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-// let intervalId;
-
 start.addEventListener('click', evt => {
     start.disabled = true;
     intervalId = setInterval(() => {
         body.style.backgroundColor = getRandomHexColor();
     }, 1000)
-
-    stop.addEventListener('click', e => {
-        start.disabled = false;
-        clearInterval(intervalId);
-    })
 })
 
+stop.addEventListener('click', e => {
+    start.disabled = false;
+    clearInterval(intervalId);
+})
